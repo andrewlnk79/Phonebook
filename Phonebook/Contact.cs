7348ba49509e61600a15dbc0d2423f8d4a08186c
Andrey Linnik <av.linnik79@ya.ru>
2023-05-13 10:33:34 +0300

    Добавьте файлы проекта.

---
+++ Phonebook/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    internal class Contact
+    {
+        public string SurName { get; set; }
+        public string PhoneNumber { get; set; }
+
+       
+        
+
+        public Contact(string surname, string phoneNumber )
+        {
+            SurName = surname;
+            PhoneNumber = phoneNumber;
+            
+        }
+    }
+}
---
+++ Phonebook/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Phonebook;
+using System.Security.Cryptography;
+
+
+
+var phoneBook = new Dictionary<string, Contact>();
+phoneBook.TryAdd("Andrey", new Contact("Андрей", "+ 79990000001"));
+
+phoneBook.TryAdd("Igor", new Contact("Игорь", "+79990000000"));
+phoneBook.TryAdd("Sergey", new Contact("Аврора", "+79991111111"));
+
+
+var sort = phoneBook.OrderBy(s => s.Value.SurName).ThenBy(s => s.Key);
+foreach (var item in sort)
+{
+    Console.WriteLine(item.Key);
+}
+
+
+
